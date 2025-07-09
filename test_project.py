3ff68b67156382daf7ac62ba7855eb2af9a02462
Tejasvi.R <teyaravi@gmail.com>
2025-07-09 15:26:59 +0530

    Add files via upload

---
+++ test_project.py
@@ -0,0 +1,32 @@
+import project
+
+def main():
+    test_seven_days()
+    test_month()
+
+def test_seven_days():
+    assert project.get_week(1) == "Sunday"
+    assert project.get_week(2) == "Monday"
+    assert project.get_week(3) == "Tuesday"
+    assert project.get_week(4) == "Wednesday"
+    assert project.get_week(5) == "Thursday"
+    assert project.get_week(6) == "Friday"
+    assert project.get_week(7) == "Saturday"
+
+
+def test_month():
+    assert project.month(1) == "January"
+    assert project.month(2) == "February"
+    assert project.month(3) == "March"
+    assert project.month(4) == "April"
+    assert project.month(5) == "May"
+    assert project.month(6) == "June"
+    assert project.month(7) == "July"
+    assert project.month(8) == "August"
+    assert project.month(9) == "September"
+    assert project.month(10) == "October"
+    assert project.month(11) == "November"
+    assert project.month(12) == "December"
+
+if __name__ == "__main__":
+    main()
